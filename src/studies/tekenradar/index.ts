344dfa1aa0efb153c2e060f179c6ca5f24122a52
Katja Schneider <katja.schneider@coneno.com>
2021-11-03 12:41:30 +0100

    Update index.ts

--- src/studies/tekenradar/index.ts
+++ src/studies/tekenradar/index.ts
@@ -1,9 +1,10 @@
 import { Study } from "../../case-editor/types/study";
 import { T0 } from "./surveys/T0";
+import { TB_adults } from "./surveys/TB_adults";
 
 export const TekenradarStudy: Study = {
     studyKey: 'tekenradar',
     surveys: [
-        T0,
+        T0, TB_adults
     ]
 }
