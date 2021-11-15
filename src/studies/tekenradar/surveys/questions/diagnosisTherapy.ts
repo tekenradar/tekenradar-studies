d460a154c43bc9190a71155bd94d87d0822e4cff
Katja Schneider <katja.schneider@coneno.com>
2021-11-15 11:54:22 +0100

    Update diagnosisTherapy.ts

--- src/studies/tekenradar/surveys/questions/diagnosisTherapy.ts
+++ src/studies/tekenradar/surveys/questions/diagnosisTherapy.ts
@@ -1,8 +1,42 @@
 import { Expression } from 'survey-engine/lib/data_types';
-import { Item } from 'case-editor-tools/surveys/types';
-import { SurveyItems } from 'case-editor-tools/surveys';
+import { Group, Item } from 'case-editor-tools/surveys/types';
+import { SurveyEngine, SurveyItems } from 'case-editor-tools/surveys';
 
-export class FormerLymeDiagnosis extends Item {
+
+
+export class FormerLymeGroup extends Group {
+
+    Q1: FormerLymeDiagnosis;
+    Q2: FormerLymeTherapy1;
+    Q3: FormerLymeTherapy2;
+
+    
+    constructor(parentKey: string,isRequired?: boolean) {
+        super(parentKey, 'FLG');
+
+
+        this.Q1 = new FormerLymeDiagnosis(this.key, false);
+        const q1Condition = SurveyEngine.singleChoice.any(this.Q1.key, this.Q1.optionKeys.nameOfOption);
+        this.Q2 = new FormerLymeTherapy1(this.key, false, q1Condition);
+        this.Q3 = new FormerLymeTherapy2(this.key, false, q1Condition);
+
+    }
+
+    buildGroup() {
+
+        this.addItem(this.Q1.get());
+        this.addItem(this.Q2.get());
+        this.addItem(this.Q3.get());
+
+    }
+}
+
+
+class FormerLymeDiagnosis extends Item {
+
+    optionKeys = {
+        nameOfOption: 'a'
+     }
 
   constructor(parentKey: string, isRequired: boolean, condition?: Expression) {
     super(parentKey, 'FLD');
@@ -18,7 +52,7 @@ export class FormerLymeDiagnosis extends Item {
       isRequired: this.isRequired,
       condition: this.condition,
       questionText: new Map([
-        ['nl', 'Heeft de huisarts bij jou ooit eerder een \'erythema migrans\' of een andere uiting van \'de ziekte van Lyme\' vastgesteld?'],
+        ['nl', 'Is er bij jou ooit eerder een erythema migrans of een andere vorm van de ziekte van Lyme vastgesteld?'],
       ]),
       responseOptions: [
         {
@@ -39,7 +73,7 @@ export class FormerLymeDiagnosis extends Item {
 }
 
 
-export class FormerLymeTherapy1 extends Item {
+class FormerLymeTherapy1 extends Item {
 
   constructor(parentKey: string, isRequired: boolean, condition?: Expression) {
     super(parentKey, 'FLTher1');
@@ -83,7 +117,7 @@ export class FormerLymeTherapy1 extends Item {
 
 
 
-export class FormerLymeTherapy2 extends Item {
+class FormerLymeTherapy2 extends Item {
 
   constructor(parentKey: string, isRequired: boolean, condition?: Expression) {
     super(parentKey, 'FLTher2');
