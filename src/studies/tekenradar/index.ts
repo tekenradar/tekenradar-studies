5f48735a157e8ec12720819926170c4eec5fbb45
Katja Schneider <katja.schneider@coneno.com>
2021-12-09 12:29:02 +0100

    Update index.ts

--- src/studies/tekenradar/index.ts
+++ src/studies/tekenradar/index.ts
@@ -1,10 +1,15 @@
 import { Study } from "case-editor-tools/types/study";
 import { T0 } from "./surveys/T0";
-import { Adults } from "./surveys/Adults";
+import { PDiff } from "./surveys/PDiff";
+import { EMflow_Adults } from "./surveys/EMflow_Adults";
+import { TBflow_Adults } from "./surveys/TBflow_Adults";
+import { LBflow_Adults } from "./surveys/LBflow_Adults";
+import { Chronicflow_Adults } from "./surveys/Chronicflow_Adults";
+import { Feverflow_Adults } from "./surveys/Feverflow_Adults";
 
 export const TekenradarStudy: Study = {
     studyKey: 'tekenradar',
     surveys: [
-        T0, Adults
+        PDiff, TBflow_Adults, EMflow_Adults, LBflow_Adults, Chronicflow_Adults, Feverflow_Adults
     ]
 }
