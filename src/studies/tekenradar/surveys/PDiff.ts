3bc410db70c151edc117f5dde463aea00a70ffce
Katja Schneider <katja.schneider@coneno.com>
2021-12-09 12:29:24 +0100

    Update PDiff.ts

--- src/studies/tekenradar/surveys/PDiff.ts
+++ src/studies/tekenradar/surveys/PDiff.ts
@@ -1,14 +1,23 @@
-import { PDiffGroup } from './questions/PDiffQuestions'
+import { DetectTickBite, EMTextPDiff, EMTickBite, FeverTickBite, IntroPDiff, LymeTickBite1, LymeTickBite2, MedicationLyme } from './questions/PDiffQuestions'
 import { SurveyDefinition } from 'case-editor-tools/surveys/types';
+import { SurveyEngine } from 'case-editor-tools/surveys';
+import { Age } from './questions/demographie';
 
-export class PDiff extends SurveyDefinition {
-
-    
-    G1: PDiffGroup;
+export class PDiffDef extends SurveyDefinition {
+ 
+  T1: IntroPDiff;
+  Q1: DetectTickBite;
+  Q2: FeverTickBite;
+  T2: EMTextPDiff;
+  Q3: EMTickBite;
+  Q4: LymeTickBite1;
+  Q5: LymeTickBite2;
+  Q6: MedicationLyme;
+  Q7: Age;
     
   
-    constructor(parentKey: string, isRequired?: boolean) {
-      super({
+  constructor(isRequired?: boolean) {
+    super({
       surveyKey: 'PDiff',
       name: new Map([
         ['en', 'Test']
@@ -21,18 +30,39 @@ export class PDiff extends SurveyDefinition {
       ]),
     });
   
-      //const required = isRequired !== undefined ? isRequired : false;
-  
-      this.G1 = new PDiffGroup(this.key, isRequired);
+    const required = isRequired !== undefined ? isRequired : false;
 
+    this.T1 = new IntroPDiff(this.key, required);
+    this.Q1 = new DetectTickBite(this.key, required);
+    const q1Condition = SurveyEngine.singleChoice.any(this.Q1.key, this.Q1.optionKeys.nameOfOption);
+
+    this.Q2 = new FeverTickBite(this.key, required, q1Condition);
+    this.T2 = new EMTextPDiff(this.key, required);
+    this.Q3 = new EMTickBite(this.key, required);
+    this.Q4 = new LymeTickBite1(this.key, required);
+    const q4Condition = SurveyEngine.singleChoice.any(this.Q4.key, this.Q4.optionKeys.nameOfOption);
+
+    this.Q5 = new LymeTickBite2(this.key, required, q4Condition);
+    this.Q6 = new MedicationLyme(this.key, required, q4Condition);
+    this.Q7 = new Age(this.key, required);
     }
   
   
     buildSurvey() {
   
-      this.addItem(this.G1.get());
-  
+      this.addItem(this.T1.get());
+      this.addItem(this.Q1.get());
+      this.addItem(this.Q2.get());
+      this.addItem(this.T2.get());
+      this.addItem(this.Q3.get());
+      this.addItem(this.Q4.get());
+      this.addItem(this.Q5.get());
+      this.addItem(this.Q6.get());
+      this.addItem(this.Q7.get());
     }
   
   }
+
+  
+export const PDiff = new PDiffDef(false);
   
