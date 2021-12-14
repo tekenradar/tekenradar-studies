a6c5916d7c0ce52c81c8f91bcda1918c6239af0c
Katja Schneider <katja.schneider@coneno.com>
2021-12-14 12:04:03 +0100

    Update Standardflow_Adults.ts

--- src/studies/tekenradar/surveys/Standardflow_Adults.ts
+++ src/studies/tekenradar/surveys/Standardflow_Adults.ts
@@ -1,13 +1,22 @@
 import { SurveyDefinition } from 'case-editor-tools/surveys/types';
 import { LymeDiagnosis2 } from './questions/diagnosisTherapy';
 import { LymeDiagnosis3, LymeDiagnosis4, LymeDiagnosis5, LymeDiagnosis6 } from './questions/lyme';
-import {FeverFU1, FeverFU2, LymeFU, MedicationFU1, MedicationFU2, NewTB, PreviousTickBites3, ReportedTB2, SymptomsFU1, SymptomsFU2, Text1FU, Text2FU } from './questions/followup';
-import { Tekenradar } from './questions/standard';
+import {FeverFU1, FeverFU2, LymeFU, MedicationFU1, MedicationFU2, NewTB, PreviousTickBites3, ReportedTB2, Text1FU, Text2FU } from './questions/followup';
+import { Qualification, Symptoms1, Symptoms2, Symptoms3, Tekenradar, Text1Standard } from './questions/standard';
+import { Residence, Gender } from './questions/demographie';
+
 
 
 class Standardflow_AdultsDef extends SurveyDefinition {
 
     Q1: Tekenradar;
+    T1: Text1Standard;
+    Q2: Qualification;
+    Q3: Residence;
+    Q4: Gender;
+    Q5: Symptoms1;
+    Q6: Symptoms2;
+    Q7: Symptoms3;
 
 
     constructor(isRequired?: boolean) {
@@ -27,6 +36,14 @@ class Standardflow_AdultsDef extends SurveyDefinition {
 
         const required = isRequired !== undefined ? isRequired : false;
         this.Q1 = new Tekenradar(this.key, required);
+        this.T1 = new Text1Standard(this.key, required);
+        this.Q2 = new Qualification(this.key, required);
+        this.Q3 = new Residence(this.key, required);
+        this.Q4 = new Gender(this.key, required);
+        this.Q5 = new Symptoms1(this.key, required);
+        this.Q6 = new Symptoms2(this.key, required);
+        this.Q7 = new Symptoms3(this.key, required);
+
 
 
     }
@@ -34,8 +51,14 @@ class Standardflow_AdultsDef extends SurveyDefinition {
     buildSurvey() {
 
         this.addItem(this.Q1.get());
+        this.addItem(this.T1.get());
+        this.addItem(this.Q2.get());
+        this.addItem(this.Q3.get());
+        this.addItem(this.Q4.get());
+        this.addItem(this.Q5.get());
+        this.addItem(this.Q6.get());
+        this.addItem(this.Q7.get());
 
-      
     }
 }
 
