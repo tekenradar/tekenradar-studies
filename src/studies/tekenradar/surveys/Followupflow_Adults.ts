a381d699df58f1815598404665ee8135f8ee56fc
Katja Schneider <katja.schneider@coneno.com>
2021-12-15 12:37:08 +0100

    Update Followupflow_Adults.ts

--- src/studies/tekenradar/surveys/Followupflow_Adults.ts
+++ src/studies/tekenradar/surveys/Followupflow_Adults.ts
@@ -3,6 +3,7 @@ import { LymeDiagnosis2 } from './questions/diagnosisTherapy';
 import { LymeDiagnosis3, LymeDiagnosis4, LymeDiagnosis5, LymeDiagnosis6 } from './questions/lyme';
 import {FeverFU1, FeverFU2, LymeFU, MedicationFU1, MedicationFU2, NewTB, PreviousTickBites3, ReportedTB2, SymptomsFU, Text1FU, Text2FU } from './questions/followup';
 import { Symptoms1 } from './questions/standard';
+import { SurveyEngine } from 'case-editor-tools/surveys';
 
 class Followupflow_AdultsDef extends SurveyDefinition {
 
@@ -44,22 +45,29 @@ class Followupflow_AdultsDef extends SurveyDefinition {
 
         this.T1 = new Text1FU(this.key, required);
         this.Q1 = new NewTB(this.key, required);
-        this.Q2 = new ReportedTB2(this.key, required);
-        this.Q3 = new PreviousTickBites3(this.key, required);
+        const Q1condition = SurveyEngine.singleChoice.any(this.Q1.key, this.Q1.optionKeys.nameOfOption);
+        this.Q2 = new ReportedTB2(this.key, required, Q1condition);
+        const Q2condition = SurveyEngine.singleChoice.any(this.Q2.key, this.Q2.optionKeys.nameOfOption);
+        const Q1_2condition = SurveyEngine.logic.and(Q1condition, Q2condition);
+        this.Q3 = new PreviousTickBites3(this.key, required, Q1_2condition);
+
+        //TODO: add 4 and 5 only for TB & EM flow at t=3months
         this.Q4 = new FeverFU1(this.key, required);
-        this.Q5 = new FeverFU2(this.key, required);
+        const Q4condition = SurveyEngine.singleChoice.any(this.Q4.key, this.Q4.optionKeys.nameOfOption);
+        this.Q5 = new FeverFU2(this.key, required, Q4condition);
 
         this.T2 = new Text2FU(this.key, required);
         this.Q6 = new LymeFU(this.key, required);
-        this.Q7 = new LymeDiagnosis2(this.key, required);
-        this.Q8 = new LymeDiagnosis3(this.key, required);
-        this.Q9 = new LymeDiagnosis4(this.key, required);
-        this.Q10 = new LymeDiagnosis5(this.key, required);
-        this.Q11 = new LymeDiagnosis6(this.key, required);
-        this.Q12 = new MedicationFU1(this.key, required);
-        this.Q13 = new MedicationFU2(this.key, required);
-        this.Q14 = new Symptoms1(this.key, required);
-        this.Q15 = new SymptomsFU(this.key, required);
+        const Q6condition = SurveyEngine.singleChoice.any(this.Q6.key, this.Q6.optionKeys.nameOfOption);
+        this.Q7 = new LymeDiagnosis2(this.key, required, Q6condition);
+        this.Q8 = new LymeDiagnosis3(this.key, required, Q6condition);
+        this.Q9 = new LymeDiagnosis4(this.key, required, Q6condition);
+        this.Q10 = new LymeDiagnosis5(this.key, required, Q6condition);
+        this.Q11 = new LymeDiagnosis6(this.key, required, Q6condition);
+        this.Q12 = new MedicationFU1(this.key, required, Q6condition);
+        this.Q13 = new MedicationFU2(this.key, required, Q6condition);
+        this.Q14 = new Symptoms1(this.key, required, Q6condition);
+        this.Q15 = new SymptomsFU(this.key, required, Q6condition);
 
     }
 
