49ad3f424cc83a3e70d2f9b59623fffb77c5450b
phev8 <phev8@users.noreply.github.com>
2021-12-21 20:26:19 +0100

    add further example survey questions

--- src/studies/tekenradar/surveys/ExampleSurvey.ts
+++ src/studies/tekenradar/surveys/ExampleSurvey.ts
@@ -1,5 +1,6 @@
 import { Item, SurveyDefinition } from 'case-editor-tools/surveys/types';
-import { SingleChoiceOptionTypes, SurveyEngine, SurveyItems , ClozeItemTypes, MultipleChoiceOptionTypes} from 'case-editor-tools/surveys';
+import { SingleChoiceOptionTypes, SurveyItems, ClozeItemTypes, MultipleChoiceOptionTypes } from 'case-editor-tools/surveys';
+import { generateLocStrings } from 'case-editor-tools/surveys/utils/simple-generators';
 
 class SingleChoiceExample extends Item {
   constructor(parentKey: string, isRequired: boolean) {
@@ -64,7 +65,7 @@ class SingleChoiceOptionExample extends Item {
           key: 'number',
           inputLabel: new Map([['nl', 'Option with number input:']]),
         }),
-       //TODO: Peter: date input as option only with ClozeItemTypes?
+        //TODO: Peter: date input as option only with ClozeItemTypes?
       ]
     })
   }
@@ -242,7 +243,7 @@ class MultipleChoiceOptionExample extends Item {
           key: 'number',
           inputLabel: new Map([['nl', 'Option with number input:']]),
         }),
-       //TODO: Peter: date input as option only with ClozeItemTypes?
+        //TODO: Peter: date input as option only with ClozeItemTypes?
       ]
     })
   }
@@ -416,10 +417,8 @@ class DateInputExample extends Item {
 
 
 export class NumberInputExample extends Item {
-
   constructor(parentKey: string, isRequired: boolean) {
     super(parentKey, 'NInput');
-
     this.isRequired = isRequired;
   }
 
@@ -440,12 +439,271 @@ export class NumberInputExample extends Item {
       componentProperties: {
         min: 0,
         max: 120
-      }
+      },
     })
   }
 }
 
 
+export class LikertExample extends Item {
+  constructor(parentKey: string, isRequired: boolean) {
+    super(parentKey, 'LIKERT');
+    this.isRequired = isRequired;
+  }
+
+  buildItem() {
+    return SurveyItems.old.simpleLikertGroup({
+      parentKey: this.parentKey,
+      itemKey: this.itemKey,
+      isRequired: this.isRequired,
+      condition: this.condition,
+      questionText: new Map([
+        ['nl', 'Question with single likert:'],
+      ]),
+      scaleOptions: [
+        { key: 'o1', content: new Map([['nl', '1']]) },
+        { key: 'o2', content: new Map([['nl', '2']]) },
+        { key: 'o3', content: new Map([['nl', '3']]) },
+        { key: 'o4', content: new Map([['nl', '4']]) },
+        { key: 'o5', content: new Map([['nl', '5']]) },
+      ],
+      rows: [
+        { key: 'row1', content: new Map([['nl', 'Row 1 label']]) },
+        { key: 'row2', content: new Map([['nl', 'Row 2 label']]) },
+      ]
+    })
+  }
+}
+
+export class ClozeExample extends Item {
+  constructor(parentKey: string, isRequired: boolean) {
+    super(parentKey, 'CLZ');
+    this.isRequired = isRequired;
+  }
+
+  buildItem() {
+    return SurveyItems.clozeQuestion({
+      parentKey: this.parentKey,
+      itemKey: this.itemKey,
+      isRequired: this.isRequired,
+      condition: this.condition,
+      questionText: new Map([
+        ['nl', 'Question with cloze:'],
+      ]),
+      items: [
+        ClozeItemTypes.text({ content: new Map([['nl', 'dateinput: ']]) }),
+        ClozeItemTypes.dateInput({ key: 'date', dateInputMode: 'YMD' }),
+        ClozeItemTypes.text({ content: new Map([['nl', 'dropdown: ']]) }),
+        ClozeItemTypes.dropDown({
+          key: 'dropdown',
+          options: [
+            { key: '1', role: 'option', content: new Map([['nl', 'Option 1']]) },
+            { key: '2', role: 'option', content: new Map([['nl', 'Option 2']]) },
+            { key: '3', role: 'option', content: new Map([['nl', 'Option 3']]) },
+          ]
+        }),
+        ClozeItemTypes.clozeLineBreak(),
+        ClozeItemTypes.text({ content: new Map([['nl', 'text: ']]) }),
+        ClozeItemTypes.textInput({ key: 'text', }),
+        ClozeItemTypes.text({ content: new Map([['nl', '']]) }),
+        ClozeItemTypes.numberInput({ key: 'number', inputLabel: new Map([]) }),
+      ]
+    })
+  }
+}
+
+
+export class DateExample extends Item {
+  constructor(parentKey: string, isRequired: boolean) {
+    super(parentKey, 'DATE');
+    this.isRequired = isRequired;
+  }
+
+  buildItem() {
+    return SurveyItems.dateInput({
+      parentKey: this.parentKey,
+      itemKey: this.itemKey,
+      isRequired: this.isRequired,
+      condition: this.condition,
+      questionText: new Map([
+        ['nl', 'Question with date:'],
+      ]),
+      dateInputMode: 'YM',
+    })
+  }
+}
+
+export class SliderExample extends Item {
+  constructor(parentKey: string, isRequired: boolean) {
+    super(parentKey, 'SLIDER');
+    this.isRequired = isRequired;
+  }
+
+  buildItem() {
+    return SurveyItems.numericSlider({
+      parentKey: this.parentKey,
+      itemKey: this.itemKey,
+      isRequired: this.isRequired,
+      condition: this.condition,
+      questionText: new Map([
+        ['nl', 'Question with numeric slider:'],
+      ]),
+      noResponseLabel: new Map([['nl', 'Select']]),
+      sliderLabel: new Map([['nl', 'Label']]),
+      max: 50,
+      min: 15
+    })
+  }
+}
+
+
+export class RSCAExample extends Item {
+  constructor(parentKey: string, isRequired: boolean) {
+    super(parentKey, 'RSCA');
+    this.isRequired = isRequired;
+  }
+
+  buildItem() {
+    return SurveyItems.responsiveSingleChoiceArray({
+      parentKey: this.parentKey,
+      itemKey: this.itemKey,
+      isRequired: this.isRequired,
+      condition: this.condition,
+      questionText: new Map([
+        ['nl', 'Question with responsive single choice array:'],
+      ]),
+      responsiveModes: {
+        sm: 'table',
+      },
+      scaleOptions: [
+        { key: 'o1', content: new Map([['nl', 'low']]) },
+        { key: 'o2', content: new Map([['nl', 'medium']]) },
+        { key: 'o3', content: new Map([['nl', 'high']]) },
+      ],
+      rows: [
+        { key: 'r1', content: new Map([['nl', 'Row 1']]) },
+        { key: 'r2', content: new Map([['nl', 'Row 2']]) },
+        { key: 'r3', content: new Map([['nl', 'Row 3']]) },
+      ],
+      defaultMode: 'vertical',
+    })
+  }
+}
+
+
+export class RBLAExample extends Item {
+  constructor(parentKey: string, isRequired: boolean) {
+    super(parentKey, 'RBLA');
+    this.isRequired = isRequired;
+  }
+
+  buildItem() {
+    return SurveyItems.responsiveBipolarLikertArray({
+      parentKey: this.parentKey,
+      itemKey: this.itemKey,
+      isRequired: this.isRequired,
+      condition: this.condition,
+      questionText: new Map([
+        ['nl', 'Question with responsive single choice array:'],
+      ]),
+      responsiveModes: {
+        sm: 'table',
+      },
+      scaleOptions: [
+        { key: 'o1', },
+        { key: 'o2', },
+        { key: 'o3', },
+      ],
+      rows: [
+        { key: 'r1', startLabel: new Map([['nl', 'Row 1 start']]), endLabel: new Map([['nl', 'Row 1 end']]) },
+        { key: 'r2', startLabel: new Map([['nl', 'Row 2 start']]), endLabel: new Map([['nl', 'Row 2 end']]) },
+        { key: 'r3', startLabel: new Map([['nl', 'Row 3 start']]), endLabel: new Map([['nl', 'Row 3 end']]) },
+      ],
+      defaultMode: 'vertical',
+    })
+  }
+}
+
+
+export class SurveyEndExample extends Item {
+  constructor(parentKey: string, isRequired: boolean) {
+    super(parentKey, 'END');
+    this.isRequired = isRequired;
+  }
+
+  buildItem() {
+    return SurveyItems.surveyEnd(
+      this.parentKey,
+      new Map([
+        ['nl', 'End of the survey.'],
+      ]))
+  }
+}
+
+
+export class MapExample extends Item {
+  constructor(parentKey: string, isRequired: boolean) {
+    super(parentKey, 'MAP');
+    this.isRequired = isRequired;
+  }
+
+  buildItem() {
+    return SurveyItems.customQuestion({
+      parentKey: this.parentKey,
+      itemKey: this.itemKey,
+      isRequired: this.isRequired,
+      condition: this.condition,
+      questionText: new Map([
+        ['nl', 'Question with custom map:'],
+      ]),
+      responseItemDefs: [
+        {
+          key: 'map',
+          role: 'map',
+        }
+      ]
+    })
+  }
+}
+
+export class CustomExample extends Item {
+  constructor(parentKey: string, isRequired: boolean) {
+    super(parentKey, 'CUSTOM');
+    this.isRequired = isRequired;
+  }
+
+  buildItem() {
+    return SurveyItems.customQuestion({
+      parentKey: this.parentKey,
+      itemKey: this.itemKey,
+      isRequired: this.isRequired,
+      condition: this.condition,
+      questionText: new Map([
+        ['nl', 'Question with custom question with mapping:'],
+      ]),
+      responseItemDefs: [
+        {
+          key: '1',
+          role: 'customScg',
+          mapToRole: 'singleChoiceGroup',
+          items: [
+            { key: '1', role: 'option', content: generateLocStrings(new Map([['nl', 'Option 1']])) },
+            { key: '2', role: 'option', content: generateLocStrings(new Map([['nl', 'Option 2']])) },
+          ]
+        },
+        {
+          key: '2',
+          role: 'customScg',
+          mapToRole: 'singleChoiceGroup',
+          items: [
+            { key: '1', role: 'option', content: generateLocStrings(new Map([['nl', 'Option 1']])) },
+            { key: '2', role: 'option', content: generateLocStrings(new Map([['nl', 'Option 2']])) },
+          ]
+        }
+      ]
+    })
+  }
+}
 
 
 class ExampleSurveyDef extends SurveyDefinition {
@@ -459,6 +717,16 @@ class ExampleSurveyDef extends SurveyDefinition {
   Q8: Item;
   Q9: Item;
   Q10: Item;
+  QLikert: Item;
+  QCloze: Item;
+  QDate: Item;
+  QSlider: Item;
+  QRSCA: Item;
+  QRBL: Item;
+  QMAP: Item;
+  QCustomWithMapping: Item;
+  SurveyEnd: Item;
+
 
   constructor(isRequired?: boolean) {
     super({
@@ -487,9 +755,16 @@ class ExampleSurveyDef extends SurveyDefinition {
     this.Q8 = new MultilineTextInputExample(this.key, required);
     this.Q9 = new DateInputExample(this.key, required);
     this.Q10 = new NumberInputExample(this.key, required);
-
+    this.QLikert = new LikertExample(this.key, required);
+    this.QCloze = new ClozeExample(this.key, required);
+    this.QDate = new DateExample(this.key, required);
+    this.QSlider = new SliderExample(this.key, required);
+    this.QRSCA = new RSCAExample(this.key, required);
+    this.QRBL = new RBLAExample(this.key, required);
+    this.QMAP = new MapExample(this.key, required);
+    this.QCustomWithMapping = new CustomExample(this.key, required);
+    this.SurveyEnd = new SurveyEndExample(this.key, required);
   }
-
 
   buildSurvey() {
     this.addItem(this.Q1.get());
@@ -502,12 +777,17 @@ class ExampleSurveyDef extends SurveyDefinition {
     this.addItem(this.Q8.get());
     this.addItem(this.Q9.get());
     this.addItem(this.Q10.get());
-
+    this.addItem(this.QLikert.get());
+    this.addItem(this.QCloze.get());
+    this.addItem(this.QDate.get());
+    this.addItem(this.QSlider.get());
+    this.addItem(this.QRSCA.get());
+    this.addItem(this.QRBL.get());
+    this.addItem(this.QMAP.get());
+    this.addItem(this.QCustomWithMapping.get());
+    this.addItem(this.SurveyEnd.get());
   }
-
 }
 
 
 export const ExampleSurvey = new ExampleSurveyDef(false);
-
-
