ca881184e9926e4601eee1486446d20b95ee459e
Katja Schneider <katja.schneider@coneno.com>
2021-12-23 23:49:58 +0100

    Update Followupflow_Adults.ts

--- src/studies/tekenradar/surveys/Followupflow_Adults.ts
+++ src/studies/tekenradar/surveys/Followupflow_Adults.ts
@@ -2,7 +2,7 @@ import { SurveyDefinition } from 'case-editor-tools/surveys/types';
 import { LymeDiagnosis2 } from './questions/diagnosisTherapy';
 import { LymeDiagnosis3, LymeDiagnosis4, LymeDiagnosis5, LymeDiagnosis6 } from './questions/lyme';
 import {FeverFU1, FeverFU2, LymeFU, MedicationFU1, MedicationFU2, NewTB, PreviousTickBites3, ReportedTB2, SymptomsFU, Text1FU, Text2FU } from './questions/followup';
-import { Symptoms1 } from './questions/standard';
+import { Cognition, Fatigue, Functioning1, Functioning2, Functioning3, Functioning4, Functioning5, FunctioningText, MedCare1, MedCare2, MedCareText1, MedCareText2, MedCareText3, Pregnant, Symptoms1, Symptoms2, Symptoms3 } from './questions/standard';
 import { SurveyEngine } from 'case-editor-tools/surveys';
 
 class Followupflow_AdultsDef extends SurveyDefinition {
@@ -22,8 +22,24 @@ class Followupflow_AdultsDef extends SurveyDefinition {
     Q11: LymeDiagnosis6;
     Q12: MedicationFU1;
     Q13: MedicationFU2;
-    Q14: Symptoms1;
-    Q15: SymptomsFU;
+    Q14: Symptoms2;
+    Q15: Symptoms3;
+    Q16: Pregnant;
+    T3: FunctioningText;
+    Q17: Functioning1;
+    Q18: Functioning2;
+    Q19: Functioning3;
+    Q20: Functioning4;
+    Q21: Functioning5;
+    Q22: Fatigue;
+    Q23: Cognition;
+    T4: MedCareText1;
+    Q24: MedCare1;
+    T5: MedCareText2;
+    Q25: MedCare2;
+    T6: MedCareText3;
+    Q26: Symptoms1;
+    Q27: SymptomsFU;
 
 
     constructor(isRequired?: boolean) {
@@ -65,9 +81,30 @@ class Followupflow_AdultsDef extends SurveyDefinition {
         this.Q10 = new LymeDiagnosis5(this.key, required, Q6condition);
         this.Q11 = new LymeDiagnosis6(this.key, required, Q6condition);
         this.Q12 = new MedicationFU1(this.key, required, Q6condition);
-        this.Q13 = new MedicationFU2(this.key, required, Q6condition);
-        this.Q14 = new Symptoms1(this.key, required, Q6condition);
-        this.Q15 = new SymptomsFU(this.key, required, Q6condition);
+        const Q12number =  SurveyEngine.getResponseValueAsNum(this.Q12.key,'rg.scg.b.2');
+
+        this.Q13 = new MedicationFU2(this.key, required, Q12number);
+        this.Q14 = new Symptoms2(this.key, required, Q6condition);
+        this.Q15 = new Symptoms3(this.key, required, Q6condition);
+        this.Q16 = new Pregnant(this.key, required, Q6condition);
+        this.T3 = new FunctioningText(this.key, required, Q6condition);
+        this.Q17 = new Functioning1(this.key, required, Q6condition);
+        this.Q18 = new Functioning2(this.key, required, Q6condition);
+        this.Q19 = new Functioning3(this.key, required, Q6condition);
+        this.Q20 = new Functioning4(this.key, required, Q6condition);
+        this.Q21 = new Functioning5(this.key, required, Q6condition);
+        this.Q22 = new Fatigue(this.key, required, Q6condition);
+        this.Q23 = new Cognition(this.key, required, Q6condition);
+
+        this.T4 = new MedCareText1(this.key, required,Q6condition);
+        this.Q24 = new MedCare1(this.key, required,Q6condition);
+        const Q24condition = SurveyEngine.singleChoice.any(this.Q24.key, this.Q24.optionKeys.nameOfOption);
+        this.T5 = new MedCareText2(this.key, required,Q6condition);
+        this.Q25 = new MedCare2(this.key, required, Q24condition);
+        this.T6 = new MedCareText3(this.key, required, Q24condition);
+
+        this.Q26 = new Symptoms1(this.key, required, Q6condition);
+        this.Q27 = new SymptomsFU(this.key, required, Q6condition);
 
     }
 
@@ -79,7 +116,6 @@ class Followupflow_AdultsDef extends SurveyDefinition {
         this.addItem(this.Q3.get());
         this.addItem(this.Q4.get());
         this.addItem(this.Q5.get());
-
         this.addItem(this.T2.get());
         this.addItem(this.Q6.get());
         this.addItem(this.Q7.get());
@@ -91,7 +127,23 @@ class Followupflow_AdultsDef extends SurveyDefinition {
         this.addItem(this.Q13.get());
         this.addItem(this.Q14.get());
         this.addItem(this.Q15.get());
-        
+        this.addItem(this.Q16.get());
+        this.addItem(this.T3.get());
+        this.addItem(this.Q17.get());
+        this.addItem(this.Q18.get());
+        this.addItem(this.Q19.get());
+        this.addItem(this.Q20.get());
+        this.addItem(this.Q21.get());
+        this.addItem(this.Q22.get());
+        this.addItem(this.Q23.get());
+        this.addItem(this.T4.get());
+        this.addItem(this.Q24.get());
+        this.addItem(this.T5.get());
+        this.addItem(this.Q25.get());
+        this.addItem(this.T6.get());
+        this.addItem(this.Q26.get());
+        this.addItem(this.Q27.get());
+
     }
 }
 
