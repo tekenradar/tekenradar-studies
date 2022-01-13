d449ddc699bf29326185c30b7203f7cc8fa05022
Katja Schneider <katja.schneider@coneno.com>
2022-01-13 11:10:26 +0100

    Update standard.ts

--- src/studies/tekenradar/surveys/questions/standard.ts
+++ src/studies/tekenradar/surveys/questions/standard.ts
@@ -1,6 +1,6 @@
 import { Expression } from 'survey-engine/data_types';
 import { Item, Group } from 'case-editor-tools/surveys/types';
-import { SurveyItems } from 'case-editor-tools/surveys';
+import { SurveyEngine, SurveyItems } from 'case-editor-tools/surveys';
 import { SingleChoiceOptionTypes as SCOptions, ClozeItemTypes } from 'case-editor-tools/surveys';
 import { ComponentGenerators } from 'case-editor-tools/surveys/utils/componentGenerators';
 
@@ -2346,3 +2346,71 @@ export class Awareness8 extends Item {
   }
 }
 
+
+export class QuestionsKids extends Item {
+
+  constructor(parentKey: string, isRequired: boolean, condition?: Expression) {
+    super(parentKey, 'QuKids');
+
+    this.isRequired = isRequired;
+    this.condition = condition;
+  }
+
+  buildItem() {
+    return SurveyItems.singleChoice({
+      parentKey: this.parentKey,
+      itemKey: this.itemKey,
+      isRequired: this.isRequired,
+      condition: this.condition,
+      questionText: new Map([
+        ['nl', 'Wie vult op dit moment de vragen in deze vragenlijst in?'],
+      ]),
+      responseOptions: [
+        {
+          key: 'a', role: 'option',
+          content: new Map([
+            ["nl", "Ikzelf, degene jonger dan 18 jaar"],
+          ])
+        },
+        {
+          key: 'b', role: 'input',
+          content: new Map([//TODO: insert radion button subitems here
+            ["nl", "Een ouder/verzorger van degene jonger dan 18 jaar, namelijk:"],
+          ])
+        },
+      ]
+    })
+  }
+}
+
+
+
+export class TextQUKids extends Item {
+
+  markdownContent = `
+    Let op: bovenaan de pagina staat steeds wie de vragen kan beantwoorden (zie ook hierboven)! Soms is dat degene over/voor wie de vragenlijst wordt ingevuld, soms een ouder/verzorger, en soms maakt het niet.
+    `
+
+  constructor(parentKey: string, isRequired: boolean, condition?: Expression) {
+    super(parentKey, 'TextQuKids');
+
+    this.isRequired = isRequired;
+    this.condition = condition;
+  }
+
+  buildItem() {
+    return SurveyItems.display({
+      parentKey: this.parentKey,
+      itemKey: this.itemKey,
+      condition: this.condition,
+      content: [
+        ComponentGenerators.markdown({
+          content: new Map([
+            ["nl", this.markdownContent],
+          ]),
+          className: ''
+        })
+      ]
+    })
+  }
+}
