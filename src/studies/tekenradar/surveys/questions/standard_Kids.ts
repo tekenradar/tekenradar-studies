ee570c008a95f9d357a2cd1fac183d622a287b63
Katja Schneider <katja.schneider@coneno.com>
2022-01-13 22:44:09 +0100

    Update standard_Kids.ts

--- src/studies/tekenradar/surveys/questions/standard_Kids.ts
+++ src/studies/tekenradar/surveys/questions/standard_Kids.ts
@@ -74,3 +74,158 @@ Let op: bovenaan de pagina staat steeds wie de vragen kan beantwoorden (zie ook 
     })
   }
 }
+
+
+
+export class Functioning1TextKids extends Item {
+
+  markdownContent = `
+# Functioneren
+De vragen hieronder zijn voor een ouder/verzorger.
+
+Hieronder staat een lijst van dingen die een probleem kunnen zijn voor uw kind. Kunt u ons vertellen **hoe vaak** uw kind in de **afgelopen week** met elk van deze dingen problemen heeft gehad? Vink het cijfer aan dat het beste van toepassing is. U kunt kiezen uit:
+  - **0** als het **nooit** een probleem is
+  - **1** als het **bijna nooit** een probleem is
+  - **2** als het **soms** een probleem is
+  - **3** als het **vaak** een probleem is
+  - **4** als het **bijna altijd** een probleem is
+
+Er zijn geen goede of foute antwoorden.
+    `
+
+  constructor(parentKey: string, isRequired: boolean, condition?: Expression) {
+    super(parentKey, 'kS_F1Text');
+
+    this.isRequired = isRequired;
+    this.condition = condition;
+  }
+
+  buildItem() {
+    return SurveyItems.display({
+      parentKey: this.parentKey,
+      itemKey: this.itemKey,
+      condition: this.condition,
+      content: [
+        ComponentGenerators.markdown({
+          content: new Map([
+            ["nl", this.markdownContent],
+          ]),
+          className: ''
+        })
+      ]
+    })
+  }
+}
+
+export class Func1KidsF1 extends Item {
+
+  questionTextMain = [
+    {
+        content: new Map([
+            ["nl", 'Hoe vaak heeft uw kind in de '],
+        ]),
+    },
+    {
+        content: new Map([
+            ["nl", " afgelopen week"],
+        ]),
+        className: "text-decoration-underline"
+    },
+    {
+      content: new Map([
+          ["nl", " problemen gehad met: Lichamelijk functioneren (problemen met…)"],
+      ]),
+    }
+  ]
+
+  constructor(parentKey: string, isRequired: boolean, condition?: Expression) {
+    super(parentKey, 'Func1kS_F1');
+
+    this.isRequired = isRequired;
+    this.condition = condition;
+  }
+
+  buildItem() {
+    return SurveyItems.responsiveSingleChoiceArray({
+      defaultMode: 'table',
+      parentKey: this.parentKey,
+      itemKey: this.itemKey,
+      isRequired: this.isRequired,
+      condition: this.condition,
+      questionText: this.questionTextMain,
+      scaleOptions: [
+        {
+          key: '0', content: new Map([
+            ["nl", "Nooit"],
+          ])
+        }, {
+          key: '1', content: new Map([
+            ["nl", "Bijna nooit"],
+          ])
+        }, {
+          key: '2', content: new Map([
+            ["nl", "Soms"],
+          ])
+        }, {
+          key: '3', content: new Map([
+            ["nl", "Vaak"],
+          ])
+        }, {
+          key: '4', content: new Map([
+            ["nl", "Bijna altijd"],
+          ])
+        }
+      ],
+      rows: [
+        {
+          key: 'a', content: new Map([
+            ["nl", "Meer dan één straat op en neer lopen"],
+          ])
+        },
+        {
+          key: 'b',
+          content: new Map([
+            ["nl", "Rennen"],
+          ])
+        },
+        {
+          key: 'c',
+          content: new Map([
+            ["nl", "Deelnemen aan actief spel of lichamelijke oefeningen"],
+          ])
+        },
+        {
+          key: 'd',
+          content: new Map([
+            ["nl", "Iets zwaars optillen"],
+          ]),
+        },
+        {
+          key: 'e',
+          content: new Map([
+            ["nl", "Baden"],
+          ])
+        },
+        {
+          key: 'f',
+          content: new Map([
+            ["nl", "Helpen met speelgoed opruimen"],
+          ])
+        },
+        {
+          key: 'g',
+          content: new Map([
+            ["nl", "Het hebben van pijn"],
+          ])
+        },
+        {
+          key: 'h',
+          content: new Map([
+            ["nl", "Weinig energie hebben"],
+          ])
+        },
+      ]
+    })
+  }
+}
+
