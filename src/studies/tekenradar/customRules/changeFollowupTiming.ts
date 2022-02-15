dc44d4d8cd1692c0a5ef6e29e829748400889fa0
phev8 <phev8@users.noreply.github.com>
2022-02-15 14:11:08 +0100

    add change timing rule for kids as well

--- src/studies/tekenradar/customRules/changeFollowupTiming.ts
+++ src/studies/tekenradar/customRules/changeFollowupTiming.ts
@@ -1,8 +1,12 @@
 import { StudyEngine } from "case-editor-tools/expression-utils/studyEngineExpressions";
 import { T12_Adults } from "../surveys/T12_Adults";
+import { T12_Kids } from "../surveys/T12_Kids";
 import { T3_Adults } from "../surveys/T3_Adults";
+import { T3_Kids } from "../surveys/T3_Kids";
 import { T6_Adults } from "../surveys/T6_Adults";
+import { T6_Kids } from "../surveys/T6_Kids";
 import { T9_Adults } from "../surveys/T9_Adults";
+import { T9_Kids } from "../surveys/T9_Kids";
 import { addFollowUpSurvey, removeFollowUpMessagesForSurvey } from "../utils/studyRuleUtils";
 
 export const changeFollowupTimingToNow = {
@@ -24,7 +28,23 @@ export const changeFollowupTimingToNow = {
       addFollowUpSurvey(T6_Adults.key, 0, 89),
       addFollowUpSurvey(T9_Adults.key, 0, 89),
       addFollowUpSurvey(T12_Adults.key, 0, 89),
+    ),
+    StudyEngine.ifThen(
+      // If:
+      StudyEngine.participantState.hasSurveyKeyAssigned(T3_Kids.key),
+      // Then:
+      StudyEngine.participantActions.assignedSurveys.remove(T3_Kids.key, 'all'),
+      StudyEngine.participantActions.assignedSurveys.remove(T6_Kids.key, 'all'),
+      StudyEngine.participantActions.assignedSurveys.remove(T9_Kids.key, 'all'),
+      StudyEngine.participantActions.assignedSurveys.remove(T12_Kids.key, 'all'),
+      removeFollowUpMessagesForSurvey(T3_Kids.key),
+      removeFollowUpMessagesForSurvey(T6_Kids.key),
+      removeFollowUpMessagesForSurvey(T9_Kids.key),
+      removeFollowUpMessagesForSurvey(T12_Kids.key),
+      addFollowUpSurvey(T3_Kids.key, 0, 89),
+      addFollowUpSurvey(T6_Kids.key, 0, 89),
+      addFollowUpSurvey(T9_Kids.key, 0, 89),
+      addFollowUpSurvey(T12_Kids.key, 0, 89),
     )
-
   ]
 }
