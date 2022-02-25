c90ce5650d15276fe53e646cf7cbbd09e9646a59
phev8 <phev8@users.noreply.github.com>
2022-02-25 17:58:30 +0100

    weeklyTB reminder: - send if part if weekly cohort - last submission is at least one week old - distribute over the week

--- src/studies/tekenradar/messageConfigs/weeklyTBMessage.ts
+++ src/studies/tekenradar/messageConfigs/weeklyTBMessage.ts
@@ -1,15 +1,23 @@
 import { StudyEngine } from "case-editor-tools/expression-utils/studyEngineExpressions";
 import { MessageConfig } from "case-editor-tools/types/messageConfig";
 import { ParticipantFlags } from "../participantFlags";
+import { surveyKeys } from "../surveys/globalConstants";
 import { headerOverrides } from "./common";
 
 export const weeklyTBMessage: MessageConfig = {
   sendTo: 'study-participants',
-  messageType: 'study-reminder',
-  condition: StudyEngine.participantState.hasParticipantFlagKeyAndValue(ParticipantFlags.weeklyTBreporter.key, ParticipantFlags.weeklyTBreporter.values.true), // is part of weekly cohort
+  messageType: 'weekly',
+  condition: StudyEngine.and(
+    StudyEngine.or(
+      // is part of weekly cohort
+      StudyEngine.participantState.hasParticipantFlagKeyAndValue(ParticipantFlags.weeklyTBreporter.key, ParticipantFlags.weeklyTBreporter.values.true),
+      StudyEngine.participantState.hasParticipantFlagKeyAndValue(ParticipantFlags.weeklyTBreporter.key, ParticipantFlags.weeklyTBreporter.values.init),
+    ),
+    StudyEngine.participantState.lastSubmissionDateOlderThan(StudyEngine.timestampWithOffset({ days: -7 }), surveyKeys.WeeklyTB)
+  ),
   defaultLanguage: 'nl',
   label: 'Reminder message for weekly TB reporter cohort',
-  period: { days: 7 },
+  period: { days: 1 },
   sendingTime: { hour: 1, minute: 0 },
   translations: [{
     lang: 'nl',
