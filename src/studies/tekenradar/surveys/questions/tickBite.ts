0e54f42655341dcd81adadd207a9d133a3853615
phev8 <phev8@users.noreply.github.com>
2022-03-07 17:09:22 +0100

    extend TBloc group with outside NL question

--- src/studies/tekenradar/surveys/questions/tickBite.ts
+++ src/studies/tekenradar/surveys/questions/tickBite.ts
@@ -502,6 +502,7 @@ class TickBiteLocationKnown extends Item {
     ongeveer: 'b',
     denkWeten: 'c',
     nee: 'd',
+    outsideNL: 'e'
   }
 
   constructor(parentKey: string, isRequired: boolean, condition?: Expression) {
@@ -537,6 +538,12 @@ class TickBiteLocationKnown extends Item {
           key: this.optionKeys.denkWeten, role: 'option',
           content: new Map([
             ["nl", "Ja, ik denk het te weten"],
+          ])
+        },
+        {
+          key: this.optionKeys.outsideNL, role: 'option',
+          content: new Map([
+            ["nl", "Ja, maar het is buiten Nederland"],
           ])
         },
         {
@@ -578,9 +585,32 @@ class TickBiteMap extends Item {
   }
 }
 
+class TBOutsideNL extends Item {
+  constructor(parentKey: string, isRequired: boolean, condition?: Expression) {
+    super(parentKey, 'Q2b');
+
+    this.isRequired = isRequired;
+    this.condition = condition;
+  }
+
+  buildItem() {
+    return SurveyItems.textInput({
+      parentKey: this.parentKey,
+      itemKey: this.itemKey,
+      isRequired: this.isRequired,
+      condition: this.condition,
+      questionText: new Map([
+        ['nl', 'Waar heb je de tekenbeet opgelopen?'],
+      ]),
+    })
+  }
+}
+
+
 export class TickBiteLocationGroup extends Group {
   Q1: TickBiteLocationKnown;
   Q2: TickBiteMap;
+  Q2b: TBOutsideNL;
 
   constructor(parentKey: string, isRequired?: boolean, condition?: Expression) {
     super(parentKey, 'TBLoc');
@@ -590,14 +620,17 @@ export class TickBiteLocationGroup extends Group {
 
 
     this.Q1 = new TickBiteLocationKnown(this.key, required);
-    const showMap = SurveyEngine.singleChoice.none(this.Q1.key, this.Q1.optionKeys.nee);
+    const showMap = SurveyEngine.singleChoice.any(this.Q1.key, this.Q1.optionKeys.precies, this.Q1.optionKeys.ongeveer, this.Q1.optionKeys.denkWeten);
     this.Q2 = new TickBiteMap(this.key, required, showMap);
+    const showOutsideNL = SurveyEngine.singleChoice.any(this.Q1.key, this.Q1.optionKeys.outsideNL);
+    this.Q2b = new TBOutsideNL(this.key, required, showOutsideNL);
 
   }
 
   buildGroup() {
     this.addItem(this.Q1.get());
     this.addItem(this.Q2.get());
+    this.addItem(this.Q2b.get());
   }
 }
 
