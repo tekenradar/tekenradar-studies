ee7cf7d8845d9d1c2a1e1a35a3e5e9c540347e6a
margrietharms <margriet.harms@rivm.nl>
2023-06-21 01:13:29 +0200

    updated NMG flag rules

--- src/studies/tekenradar/studyRules.ts
+++ src/studies/tekenradar/studyRules.ts
@@ -435,6 +435,36 @@ const handleSubmit_Chronicflow_Kids = StudyEngine.ifThen(
   StudyEngine.participantActions.reports.setReportIcon(reports.ChronicReport.key, reports.ChronicReport.key)
 );
 
+// MH create list of postalcodes for NGM flag
+const postalCodes = ['3970', '3971', '3972', '3958', '3940', '3941', '3950', '3951', '3953', '3956', '3959', '3760', '3761', '3762', '3763', '3764', '3765', '3766', '3768', '3769', '3910', '3911',
+  '3912', '3921', '3922', '3770', '3771', '3772', '3773', '3774', '3775', '3776', '3780', '3781', '3784', '3785', '3790', '3792', '3794', '3886', '4000', '4001', '4002', '4003', '4004', '4005', '4006',
+  '4007', '4013', '4014', '4017', '4062', '4115', '4112', '4116', '4024', '4117', '4030', '4031', '4016', '4012', '4033', '4020', '4021', '4032', '4119', '4023', '4010', '4011', '4110', '4111', '6669',
+  '4040', '4041', '4050', '4051', '4043', '4053', '4054', '4147', '4170', '4171', '4161', '4211', '4212', '4214', '4153', '4155', '4156', '4157', '4158', '4185', '4175', '4063', '4174', '4182', '4060',
+  '4061', '4184', '4176', '4064', '4180', '4181', '4151', '4152', '4190', '4191', '4194', '4196', '4197', '6663', '6500', '6501', '6503', '6504', '6511', '6512', '6515', '6521', '6522', '6523', '6524',
+  '6525', '6531', '6532', '6533', '6534', '6535', '6536', '6537', '6538', '6541', '6542', '6543', '6544', '6545', '6546', '6564', '6550', '6551', '6641', '6642', '6644', '6645', '6566', '6560', '6561',
+  '6562', '6573', '6577', '6579', '6578', '6576', '6575', '6574', '6570', '6571', '6572', '6582', '6600', '6601', '6602', '6603', '6604', '6605', '6626', '6628', '6629', '6658', '6657', '6620', '6621',
+  '6627', '6659', '6654', '6653', '6650', '6651', '6652', '6631', '6655', '6673', '6665', '3920', '6660', '6661', '6662', '6672', '6674', '6666', '6676', '6678', '6668', '6677', '6675', '6670', '6671',
+  '6850', '6851', '6852', '6680', '6681', '6690', '6691', '6687', '6686', '6685', '6684', '6700', '6701', '6702', '6703', '6704', '6705', '6706', '6707', '6708', '6709', '6710', '6711', '6712', '6713',
+  '6714', '6715', '6716', '6717', '6718', '6740', '6741', '6720', '6721', '6730', '6731', '6732', '6744', '6745', '6733', '7352', '6877', '6800', '6801', '6802', '6803', '6811', '6812', '6813', '6814',
+  '6815', '6816', '6821', '6822', '6823', '6824', '6825', '6826', '6827', '6828', '6831', '6832', '6833', '6834', '6835', '6836', '6841', '6842', '6843', '6844', '6845', '6846', '6865', '6866', '6869',
+  '6874', '6860', '6861', '6862', '6870', '6871', '6880', '6881', '6882', '6883', '6990', '6991', '6994', '6955', '6950', '6951', '6952', '6953', '6956', '6957', '6891', '6900', '6901', '6902', '6903',
+  '6904', '6905', '6909', '6986', '6987', '6988', '6913', '6914', '6915', '6911', '6917', '6916', '6920', '6921', '6922', '6923', '6924', '6930', '6931', '6932', '6980', '6981', '6982', '6983', '6984',
+  '5966', '5872', '5871', '5766', '5970', '5971', '5963', '5960', '5961', '5973', '5962', '5964', '5975', '5976', '5977', '5864', '5866', '5865', '5863', '5862', '5860', '5861', '5811', '5812', '5809',
+  '5815', '5817', '5800', '5801', '5802', '5803', '5804', '5814', '5816', '5813', '5808', '5807', '5854', '5851', '5853', '5855', '5856', '6587', '6584', '6585', '6886', '6596', '6595', '6599', '6590',
+  '6591', '6598', '5000', '5001', '5002', '5003', '5004', '5011', '5012', '5013', '5014', '5015', '5017', '5018', '5021', '5022', '5025', '5026', '5032', '5035', '5036', '5037', '5038', '5041', '5042',
+  '5043', '5044', '5045', '5046', '5047', '5048', '5049', '5056', '5057', '5070', '5071', '5050', '5051', '5052', '5053', '5133', '5200', '5201', '5202', '5203', '5211', '5212', '5213', '5215', '5216',
+  '5221', '5222', '5223', '5224', '5231', '5232', '5233', '5234', '5235', '5236', '5237', '5240', '5241', '5242', '5243', '5244', '5245', '5246', '5247', '5248', '5249', '5390', '5391', '5392', '5381',
+  '5382', '5383', '5258', '5275', '5290', '5291', '5292', '5293', '5294', '5270', '5271', '5272', '5260', '5261', '5262', '5263', '5264', '5266', '5280', '5281', '5282', '5283', '5298', '5340', '5341',
+  '5342', '5343', '5344', '5345', '5346', '5347', '5348', '5349', '5350', '5351', '5366', '5367', '5368', '9750', '5370', '5371', '5373', '5352', '5358', '5353', '5354', '5355', '5356', '5357', '5359',
+  '5397', '5396', '5394', '5398', '5395', '5386', '5364', '5438', '5360', '5361', '5363', '5400', '5401', '5402', '5403', '5404', '5405', '5406', '5408', '5409', '5420', '5421', '5422', '5760', '5761',
+  '5763', '5423', '5425', '5764', '5424', '5427', '5428', '5437', '5430', '5431', '5432', '5443', '5433', '5439', '5435', '5434', '5835', '5830', '5831', '5826', '5824', '5823', '5441', '5825', '5447',
+  '5836', '5820', '5821', '5827', '5445', '5846', '5840', '5841', '5845', '5844', '5446', '5843', '5449', '5450', '5451', '5454', '5453', '5455', '5480', '5481', '5482', '5490', '5491', '5492', '5469',
+  '5460', '5461', '5462', '5463', '5464', '5465', '5466', '5457', '5600', '5601', '5602', '5603', '5604', '5605', '5606', '5611', '5612', '5613', '5614', '5615', '5616', '5617', '5621', '5622', '5623',
+  '5624', '5625', '5626', '5627', '5628', '5629', '5631', '5632', '5633', '5641', '5642', '5643', '5644', '5645', '5646', '5647', '5651', '5652', '5653', '5654', '5655', '5656', '5657', '5658', '5690',
+  '5691', '5692', '5694', '5700', '5701', '5702', '5703', '5704', '5705', '5706', '5707', '5708', '5709', '5735', '5740', '5741', '5737', '5738', '5750', '5751', '5752', '5753', '5754', '5756', '5757',
+  '5758', '5759']
+
+
 /**
  *
  */
@@ -504,19 +534,38 @@ const handleSubmit_T0_Invites = StudyEngine.ifThen(
     // Else:
     StudyEngine.participantActions.updateFlag(ParticipantFlags.consents.additionalStudies.key, ParticipantFlags.consents.additionalStudies.values.rejected)
   ),
+  //  StudyEngine.if(
+  //    StudyEngine.or(
+  //      StudyEngine.hasResponseKeyWithValue(T0_Invites.aEMInviteGroup.Contactgegevens.PC4contact.key, [responseGroupKey, inputKey].join('.'), '6511'),
+  //      StudyEngine.hasResponseKeyWithValue(T0_Invites.aEMInviteGroup.Contactgegevens.PC4contact.key, [responseGroupKey, inputKey].join('.'), '6512'),
+  //      StudyEngine.singleChoice.any(T0_Invites.aEMInviteGroup.NijmegenReis.key, T0_Invites.aEMInviteGroup.NijmegenReis.optionKeys.yes)
+  //    ),
   StudyEngine.if(
-    StudyEngine.or(
-      StudyEngine.hasResponseKeyWithValue(T0_Invites.aEMInviteGroup.Contactgegevens.PC4contact.key, [responseGroupKey, inputKey].join('.'), '6511'),
-      StudyEngine.hasResponseKeyWithValue(T0_Invites.aEMInviteGroup.Contactgegevens.PC4contact.key, [responseGroupKey, inputKey].join('.'), '6512'),
-      StudyEngine.hasResponseKeyWithValue(T0_Invites.aEMInviteGroup.Contactgegevens.PC4contact.key, [responseGroupKey, inputKey].join('.'), '6521'),
-      StudyEngine.hasResponseKeyWithValue(T0_Invites.aEMInviteGroup.Contactgegevens.PC4contact.key, [responseGroupKey, inputKey].join('.'), '6522'),
-      StudyEngine.singleChoice.any(T0_Invites.aEMInviteGroup.NijmegenReis.key, T0_Invites.aEMInviteGroup.NijmegenReis.optionKeys.yes)
-    ),
+    StudyEngine.or(...postalCodes.map(postalCode => StudyEngine.hasResponseKeyWithValue(T0_Invites.aEMInviteGroup.Contactgegevens.PC4contact.key, [responseGroupKey, inputKey].join('.'), postalCode)),
+      StudyEngine.singleChoice.any(T0_Invites.aEMInviteGroup.NijmegenReis.key, T0_Invites.aEMInviteGroup.NijmegenReis.optionKeys.yes)),
     StudyEngine.participantActions.updateFlag(ParticipantFlags.NMG.key, ParticipantFlags.NMG.values.true),
-  ),
-  StudyEngine.participantActions.externalEventHandler(researcherBackendNames.T0_Invites),
-  reAssignWeeklyToTheEndOfList(),
-);
+  ))
+
+
+//I think this might work. Explained:
+//- IF condition true, update flag
+//- condition => any of the postal codes match OR the single choice question has the answer yes
+//(not sure if this is the correct condition, feel free to modify)
+//- the syntax with the three dots, and map:
+//- three dots (…): take the array/list and insert here one by one (saves you writing it 600 times)
+//- .map ( postalCodes.map ) -> returns a list of the same size but replaces each item by: iterate over the list of postal codes, and instead each of the entries (4 digit string), use the exrpession as defined (response has a specific key and value (current postal code)
+
+//I did not test this at the moment.
+
+
+
+
+
+
+
+
+
+
 
 /**
  *
