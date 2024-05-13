41e84ea37f528967b3ec2e5244552210cf60fa02
margrietharms <margriet.harms@rivm.nl>
2024-05-13 10:30:33 +0200

    Create CovidQuestions.ts
    Separate set for Covid Questions

---
+++ src/studies/tekenradar/surveys/questions/CovidQuestions.ts
@@ -0,0 +1,197 @@
+import { Expression } from 'survey-engine/data_types';
+import { Item } from 'case-editor-tools/surveys/types';
+import { SurveyItems } from 'case-editor-tools/surveys';
+import { ComponentGenerators } from 'case-editor-tools/surveys/utils/componentGenerators';
+
+
+export class CovidHeader extends Item {
+
+  markdownContent = `
+# Covid 19
+
+    `
+
+  constructor(parentKey: string, isRequired: boolean, condition?: Expression) {
+    super(parentKey, 'Covid19');
+
+    this.isRequired = isRequired;
+    this.condition = condition;
+  }
+
+  buildItem() {
+    return SurveyItems.display({
+      parentKey: this.parentKey,
+      itemKey: this.itemKey,
+      condition: this.condition,
+      content: [
+        ComponentGenerators.markdown({
+          content: new Map([
+            ["nl", this.markdownContent],
+          ]),
+          className: ''
+        })
+      ]
+    })
+  }
+}
+
+export class Covid1 extends Item {
+
+  constructor(parentKey: string, isRequired: boolean, condition?: Expression) {
+    super(parentKey, 'COV1');
+
+    this.isRequired = isRequired;
+    this.condition = condition;
+  }
+
+  buildItem() {
+    return SurveyItems.singleChoice({
+      parentKey: this.parentKey,
+      itemKey: this.itemKey,
+      isRequired: this.isRequired,
+      condition: this.condition,
+      questionText: new Map([
+        ['nl', 'Heb je in de afgelopen 5 jaar last gehad van langdurige klachten na een infectie met het coronavirus (COVID19)?'],
+      ]),
+      responseOptions: [
+        {
+          key: 'a', role: 'option',
+          content: new Map([
+            ["nl", "Ja"],
+          ])
+        },
+        {
+          key: 'b', role: 'option',
+          content: new Map([
+            ["nl", "Nee"],
+          ])
+        },
+      ]
+    })
+  }
+}
+
+export class Covid2 extends Item {
+  constructor(parentKey: string, required: boolean, condition?: Expression) {
+    super(parentKey, 'COV2');
+
+    this.condition = condition;
+    this.isRequired = required;
+  }
+
+  buildItem() {
+    return SurveyItems.dateInput({
+      parentKey: this.parentKey,
+      itemKey: this.itemKey,
+      condition: this.condition,
+      isRequired: this.isRequired,
+      questionText: new Map([[
+        'nl', 'Wanneer kreeg je de coronaviros infectie, waarna je langdurig klachten hield? (je mag de datum schatten)'
+      ]]),
+      confidentialMode: "replace",
+      dateInputMode: 'YMD',
+      maxRelativeDate: { delta: { days: 0 } },
+      // minRelativeDate: { delta: { years: -19 } },
+    })
+  }
+}
+
+export class Covid3 extends Item {
+
+
+  questionTextMain = [
+    {
+      content: new Map([
+        ["nl", 'Welke langdurige klachten had of heb je?'],
+      ]),
+      className: "row"
+    },
+    {
+      content: new Map([
+        ["nl", "Geef hier een beknopte beschrijving van je  klachten, sinds wanneer je hier last van hebt en of dit dagelijks of periodiek is."],
+      ]),
+      className: "row"
+    },
+  ]
+
+  constructor(parentKey: string, isRequired: boolean, condition?: Expression) {
+    super(parentKey, 'COV3');
+
+    this.isRequired = isRequired;
+    this.condition = condition;
+  }
+  //TODO: size of text input field?
+  buildItem() {
+    return SurveyItems.multilineTextInput({
+      parentKey: this.parentKey,
+      itemKey: this.itemKey,
+      isRequired: this.isRequired,
+      condition: this.condition,
+      questionText: this.questionTextMain
+    })
+  }
+}
+
+
+export class Covid4 extends Item {
+
+  constructor(parentKey: string, isRequired: boolean, condition?: Expression) {
+    super(parentKey, 'COV4');
+
+    this.isRequired = isRequired;
+    this.condition = condition;
+  }
+
+  buildItem() {
+    return SurveyItems.singleChoice({
+      parentKey: this.parentKey,
+      itemKey: this.itemKey,
+      isRequired: this.isRequired,
+      condition: this.condition,
+      questionText: new Map([
+        ['nl', 'Heb je nog steeds langdurige klachten na een infectie met het coronavirus?'],
+      ]),
+      responseOptions: [
+        {
+          key: 'a', role: 'option',
+          content: new Map([
+            ["nl", "Ja"],
+          ])
+        },
+        {
+          key: 'b', role: 'option',
+          content: new Map([
+            ["nl", "Nee"],
+          ])
+        },
+      ]
+    })
+  }
+}
+
+
+export class Covid5 extends Item {
+  constructor(parentKey: string, required: boolean, condition?: Expression) {
+    super(parentKey, 'COV5');
+
+    this.condition = condition;
+    this.isRequired = required;
+  }
+
+  buildItem() {
+    return SurveyItems.dateInput({
+      parentKey: this.parentKey,
+      itemKey: this.itemKey,
+      condition: this.condition,
+      isRequired: this.isRequired,
+      questionText: new Map([[
+        'nl', 'Vanaf wanneer waren de langdurige klachten na het coronavirus weg? (je mag de datum schatten)'
+      ]]),
+      confidentialMode: "replace",
+      dateInputMode: 'YMD',
+      maxRelativeDate: { delta: { days: 0 } },
+      // minRelativeDate: { delta: { years: -19 } },
+    })
+  }
+}
+
