ec5f6763d37a2b3155be3070df9d720bebc8734c
ltulen <84962812+ltulen@users.noreply.github.com>
2024-09-23 17:13:34 +0200

    Update NwEMLyme.ts
    textual adjustment

--- src/studies/tekenradar/surveys/questions/NwEMLyme.ts
+++ src/studies/tekenradar/surveys/questions/NwEMLyme.ts
@@ -50,7 +50,7 @@ export class NwEMLyme1 extends Item {
   questionTextMain = [
     {
       content: new Map([
-        ["nl", 'Heb je in de afgelopen jaren na het vorige onderzoek een (nieuwe) erythema migrans '],
+        ["nl", 'Heb je in de afgelopen jaren na het voorgaande LymeProspect of Tekenradar vragenlijstonderzoek een (nieuwe) erythema migrans '],
       ])
     },
     {
